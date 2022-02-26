0375b7ee089928a6ed8557f9ced522513f1ca5b7
andmansim <91722324+andmansim@users.noreply.github.com>
2022-02-26 13:22:10 +0100

    leer_fichas

--- bandera_de _Dijkstra.py
+++ bandera_de _Dijkstra.py
@@ -1,4 +1,5 @@
 #Variables
+
 fichas = "RVAARV"
 l_rojas = []
 l_verdes = []
@@ -6,24 +7,24 @@ l_azul = []
 l_sin_color = []
 fichas = list(fichas)
 print(fichas)
-def leer_fichas(f):
- 
-    for i in f:
-        if "R" in f:
-            c = f.index("R")
-            
-        if "V" in f:
-            a = f.index("V")
-            
-        if "A" in f:
-            b = f.index("A")
-        
-            #d = f.index("?")
-            #return l_sin_color.append(d)
-            
-    return (c, a, b)
-c_l, a_l, b_l = leer_fichas(fichas)
 
-print(c_l)
-print(a_l)
-print(b_l)
+#Función para leer fichas
+def leer_fichas(f):
+    for i in range(0, len(fichas)):
+        if fichas[i] == "R":
+            l_rojas.append(i) 
+        
+        if fichas[i] == "V":
+            l_verdes.append(i) 
+        
+        if fichas[i] == "A":
+            l_azul.append(i) 
+    print("Posiciones fichas rojas")
+    print(l_rojas)
+    print("Posiciones fichas verdes")
+    print(l_verdes)  
+    print("Posiciones fichas azules")
+    print(l_azul)
+
+leer_fichas(fichas)
+
