75e3a4dc154e6898f8de3deb4439af37a64dcd07
andmansim <91722324+andmansim@users.noreply.github.com>
2022-02-27 17:02:21 +0100

    función madrid

--- dicotomia_tabla.py
+++ dicotomia_tabla.py
@@ -1,21 +1,28 @@
 #prefijo toledo 45
-tabla = [['Maria ','25001'],['Zihara ','35210'],['Aruca ','45120'],['Yurima ','28205'],['Yeneva ','28150'],['Eduin ','45260'],['Diosnel ','35166'],['Rolindes ','58962'],['Yandira ','45896'],['Juan ','12365'],['Daniel ','48965'],['Jose ','45120'],['Yania ','28205'],['Jean ','28150'],['Enetz ','45260'],['Arnaitz ','35166']]
+tabla = [['Maria ','25001', '34'],['Zihara ','35210', '45'],['Aruca ','45120', '56'],['Yurima ','28205', '47'],['Yeneva ','28150', '49'],['Eduin ','45260', '20'],['Diosnel ','35166', '78'],['Rolindes ','58962', '82'],['Yandira ','45896', '34'],['Juan ','12365', '67'],['Daniel ','48965', '67'],['Jose ','45120', '19'],['Yania ','28205', '90'],['Jean ','28150', '39'],['Enetz ','45260', '45'],['Arnaitz ','35166', '49']]
 persona = []
 def toledo (t, n, c):
-    i = 0
-   
-    
+      
     if 0 <= n < len(t):
-                   
-        if t[n][1].startswith('45'):
+        if t[n][1].startswith(c):
             persona.append (t[n][0]) 
             toledo(t,n+1,c)
         else:
             toledo(t,n+1,c)
-            
+                
 
-
-
-    
+def Madrid (t, n, c,e,f):
+          
+    if 0 <= n < len(t):
+        if t[n][1].startswith(c):
+            if e <= t[n][2] <= f:
+                persona.append (t[n][0]) 
+                Madrid(t,n+1,c, e, f)
+        else:
+            Madrid(t,n+1,c, e, f)
+                
 toledo(tabla, 0, '45')
+print (persona)
+persona=[]
+Madrid(tabla, 0, '28','40','50')
 print (persona)
