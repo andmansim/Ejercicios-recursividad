e4f8f1edf97c2ffb218ba49002fc170918e84610
andmansim <91722324+andmansim@users.noreply.github.com>
2022-02-27 22:06:00 +0100

    terminado

--- bandera_de _Dijkstra.py
+++ bandera_de _Dijkstra.py
@@ -1,12 +1,14 @@
  #Variables
-fichas = "RVVARVVARVARAVR"
+fichas = "RVVARVVARVARAVRA"
 l_rojas = []
 l_verdes = []
 l_azul = []
 
+q = 0
+colocadas = False
+
 fichas = list(fichas)
 print(fichas)
-
 
      
 def leer_fichas(f):
@@ -28,39 +30,46 @@ def leer_fichas(f):
     print(l_azul)
 
 
-def ordenar_fichas(f):    
 
+def ordenar_fichas(q):    
     # Número de fichas de cada color 
     j = len(l_rojas)
     k = len(l_verdes)
     w = len(l_azul)
-
-    for q in range(0, len(fichas)):
-        if q <= j - 1: #en este hueco van las rojas
-                 
-            if q in l_verdes: #si lo tiene las verdes, los intercambiamos
-                p = l_verdes.index(q) #hay una roja donde las verdes, las intercambio
-           
-                fichas[l_rojas[q]], fichas[q] = fichas[q], fichas[l_rojas[q]]
-           
-                l_verdes[p] = l_rojas[q]
-            elif q in l_azul:
-                p = l_azul.index(q) #hay una roja donde las verdes, las intercambio
-            
-                fichas[l_rojas[q]], fichas[q] = fichas[q], fichas[l_rojas[q]]
-            
-                l_azul[p] = l_rojas[q]        
-            l_verdes.sort()
-            l_azul.sort()    
-                       
-        elif (k + j ) > q >= j   :  #en este hueco van las verdes
+    
+    if q <= j - 1: #en este hueco van las rojas
         
-            if q in l_azul: #si lo tiene las azules, los intercambiamos
-                p = l_azul.index(q) #hay una verde donde las azules, las intercambio 
-                fichas[l_verdes[q - j ]], fichas[q] = fichas[q], fichas[l_verdes[q - j ]]
-                l_azul[p] = l_verdes[q - j ]
+        if q in l_verdes: #si lo tiene las verdes, los intercambiamos
+            p = l_verdes.index(q) #hay una roja donde las verdes, las intercambio
+        
+            fichas[l_rojas[q]], fichas[q] = fichas[q], fichas[l_rojas[q]]
+        
+            l_verdes[p] = l_rojas[q]
+        elif q in l_azul:
+            p = l_azul.index(q) #hay una roja donde las verdes, las intercambio
+        
+            fichas[l_rojas[q]], fichas[q] = fichas[q], fichas[l_rojas[q]]
+        
+            l_azul[p] = l_rojas[q]        
+        l_verdes.sort()
+        l_azul.sort()    
+                    
+    elif (k + j ) > q >= j   :  #en este hueco van las verdes
+        
+        if q in l_azul: #si lo tiene las azules, los intercambiamos
+            p = l_azul.index(q) #hay una verde donde las azules, las intercambio 
+            fichas[l_verdes[q - j]], fichas[q] = fichas[q], fichas[l_verdes[q - j ]]
+            l_azul[p] = l_verdes[q - j]
+        
+    
+    while q < j + k :
+        q += 1
+        ordenar_fichas(q)    
+    
+       
+        
 leer_fichas(fichas)
-ordenar_fichas(fichas)
+ordenar_fichas(0)
         
 print (fichas)
     
