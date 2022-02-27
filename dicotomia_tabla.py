1e78f36ebea21dceadb1cdc31d243d3200903155
andmansim <91722324+andmansim@users.noreply.github.com>
2022-02-27 14:10:39 +0100

    función

---
+++ dicotomia_tabla.py
@@ -0,0 +1,11 @@
+#prefijo toledo 45
+tabla = [['Maria ',25001],['Zihara ',35210],['Aruca ',45120],['Yurima ',28205],['Yeneva ',28150],['Eduin ',45260],['Diosnel ',35166],['Rolindes ',58962],['Yandira ',45896],['Juan ',12365],['Daniel ',48965],['Jose ',45120],['Yania ',28205],['Jean ',28150],['Enetz ',45260],['Arnaitz ',35166]]
+def toledo (t, n, c):
+    i = 0
+    if min(t) < n < max(t):
+        i=i+1
+        if t([n],1)=c:
+            perdona= persona + toledo(t,i+1,c)
+    else:
+        persona=persona        
+        
