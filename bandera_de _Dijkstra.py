d72e2071f0c05792d4fccfff1385e787317d8a44
andmansim <91722324+andmansim@users.noreply.github.com>
2022-02-27 12:10:52 +0100

    intento mover

--- bandera_de _Dijkstra.py
+++ bandera_de _Dijkstra.py
@@ -4,7 +4,7 @@ fichas = "RVAARV"
 l_rojas = []
 l_verdes = []
 l_azul = []
-l_sin_color = []
+
 fichas = list(fichas)
 print(fichas)
 
@@ -25,10 +25,36 @@ def leer_fichas(f):
     print(l_verdes)  
     print("Posiciones fichas azules")
     print(l_azul)
-
+    
 leer_fichas(fichas)
 
-def mover_fichas():
-    for j in l_rojas:
-        if l_rojas[j] in range(0, len(l_rojas)):
+# Número de fichas de cada color 
+j = len(l_rojas)
+k = len(l_verdes)
+w = len(l_azul)
+
+for q in range(0, len(fichas)):
+    if q <= j-1: #en este hueco van las rojas
+                 
+        if q in l_verdes: #si lo tiene las verdes, los intercambiamos
+            p = l_verdes.index(q) #hay una roja donde las verdes, las intercambio
+            print (p)
+            fichas[l_rojas[q]], fichas[q] = fichas[q], fichas[l_rojas[q]]
+            print (fichas)
+            l_verdes[p] = l_rojas[q]
+        elif q in l_azul:
+            p = l_azul.index(q) #hay una roja donde las verdes, las intercambio
+            print (p)
+            fichas[l_rojas[q]], fichas[q] = fichas[q], fichas[l_rojas[q]]
+            print (fichas)
+            l_azul[p] = l_rojas[q]
             
+    elif j <= q < ((k + j) - 1):  #en este hueco van las verdes
+        if q in l_azul: #si lo tiene las azules, los intercambiamos
+            p = l_azul.index(q) #hay una verde donde las azules, las intercambio
+            print (p)
+            fichas[l_verdes[q]], fichas[q] = fichas[q], fichas[l_verdes[q]]
+            print (fichas)
+            l_azul[p] = l_verdes[q]
+        
+print (fichas)
