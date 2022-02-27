8697bba2ad64ef693a93b62a981562345123bc93
andmansim <91722324+andmansim@users.noreply.github.com>
2022-02-27 12:35:59 +0100

    función mover

--- bandera_de _Dijkstra.py
+++ bandera_de _Dijkstra.py
@@ -1,6 +1,6 @@
 #Variables
 
-fichas = "RVAARV"
+fichas = "RVVARVVARVARAVR"
 l_rojas = []
 l_verdes = []
 l_azul = []
@@ -19,6 +19,7 @@ def leer_fichas(f):
         
         if fichas[i] == "A":
             l_azul.append(i) 
+    
     print("Posiciones fichas rojas")
     print(l_rojas)
     print("Posiciones fichas verdes")
@@ -34,27 +35,33 @@ k = len(l_verdes)
 w = len(l_azul)
 
 for q in range(0, len(fichas)):
-    if q <= j-1: #en este hueco van las rojas
+    if q <= j - 1: #en este hueco van las rojas
                  
         if q in l_verdes: #si lo tiene las verdes, los intercambiamos
             p = l_verdes.index(q) #hay una roja donde las verdes, las intercambio
-            print (p)
+           
             fichas[l_rojas[q]], fichas[q] = fichas[q], fichas[l_rojas[q]]
-            print (fichas)
+           
             l_verdes[p] = l_rojas[q]
         elif q in l_azul:
             p = l_azul.index(q) #hay una roja donde las verdes, las intercambio
-            print (p)
-            fichas[l_rojas[q]], fichas[q] = fichas[q], fichas[l_rojas[q]]
-            print (fichas)
-            l_azul[p] = l_rojas[q]
             
-    elif (k + j) > q >= j  :  #en este hueco van las verdes
+            fichas[l_rojas[q]], fichas[q] = fichas[q], fichas[l_rojas[q]]
+            
+            l_azul[p] = l_rojas[q]
+        print(fichas)
+        
+        l_verdes.sort()
+        l_azul.sort()    
+        print(l_verdes)
+        print(l_azul)        
+    elif (k + j ) > q >= j   :  #en este hueco van las verdes
+        
         if q in l_azul: #si lo tiene las azules, los intercambiamos
             p = l_azul.index(q) #hay una verde donde las azules, las intercambio
-            print (p)
-            fichas[l_verdes[q - j]], fichas[q] = fichas[q], fichas[l_verdes[q - j]]
-            print (fichas)
-            l_azul[p] = l_verdes[q - j]
+            
+            fichas[l_verdes[q - j ]], fichas[q] = fichas[q], fichas[l_verdes[q - j ]]
+            
+            l_azul[p] = l_verdes[q - j ]
         
 print (fichas)
